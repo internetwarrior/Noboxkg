6d1d9f453d3c9de89a6fcb3451a08aa5d1f170c3
Your Name <you@example.com>
2024-08-28 12:28:05 +0000

    something

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),  # Use an empty string for the root URL
     path('', include('post.urls')),  
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
