20dc7fdadec9b9ea0f8b053a43d02df23969fe3b
internetwarrior <kairatbektov@gmail.com>
2024-08-31 15:21:33 +0600

    fix

--- post/models.py
+++ post/models.py
@@ -21,11 +21,15 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(max_length=500)
-    picture = models.CharField(max_length=255)
+    picture = models.ImageField(upload_to='post_img/')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     state = models.CharField(max_length=20, choices=STATE_CHOICES, default='being_checked')
+    
     created = models.DateTimeField(default=timezone.now)
+    
     updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-created']
 
     def __str__(self):
         return f"Post ID: {self.id} - {self.description[:20]}"
