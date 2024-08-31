2fa7c12cd311c412c0acac3d7145eb44da3a302b
internetwarrior <kairatbektov@gmail.com>
2024-08-31 09:29:48 +0600

    html+models fix

--- post/models.py
+++ post/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.conf import settings
+from django.utils import timezone
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
@@ -7,7 +8,7 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
-from django.conf import settings
+
 
 class Post(models.Model):
     STATE_CHOICES = [
@@ -19,10 +20,12 @@ class Post(models.Model):
 
     tags = models.ManyToManyField(Tag)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.CharField(max_length=250)
+    description = models.TextField(max_length=500)
     picture = models.CharField(max_length=255)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     state = models.CharField(max_length=20, choices=STATE_CHOICES, default='being_checked')
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Post ID: {self.id} - {self.description[:20]}"
