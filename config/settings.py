97b8749dbce13cf0563e5e71b62a6ab42602f26d
internetwarrior <kairatbektov@gmail.com>
2024-09-01 04:09:33 +0600

    s

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
-"""
+"""  
 
 import os
 
@@ -84,22 +84,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Noboxkg',
-        'USER': 'nobox',
-        'PASSWORD': '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',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
