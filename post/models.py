b4d0c5456d1727befd1c861d31be9ae2e9fd4f0e
Your Name <you@example.com>
2024-09-01 14:44:21 +0000

    Hello

--- post/models.py
+++ post/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
-
+    is_admins = models.BooleanField(default=False)
     def __str__(self):
         return self.name
 
