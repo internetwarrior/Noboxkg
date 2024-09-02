38484d06933db9f4658f00360f54de8a7cc89768
internetwarrior <kairatbektov@gmail.com>
2024-09-02 13:19:57 +0600

    redesign

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from .views import home_view,post_detail
-from django.conf import settings
-from django.conf.urls.static import static
-
+from .views import home_view,post_detail,tiktok
 
 urlpatterns = [
+    path('/tiktok',tiktok,name = "tiktok"),
     path('', home_view, name='home'),
-    path('post/<int:pk>/', post_detail, name='post_detail'),
-    ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('post/<int:pk>/', post_detail, name='post_detail')
+    ]
