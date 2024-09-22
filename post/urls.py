00e0ccd2163b12c2decca30af17a8ebc351f7acc
Your Name <you@example.com>
2024-09-22 03:08:18 +0000

    server

--- post/urls.py
+++ post/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import home_view,post_detail,tiktok,privacy
 
 urlpatterns = [
-    path('privacy/',privacy,name = "privacy_page"),
     path('tiktok/',tiktok,name = "tiktok"),
     path('', home_view, name='home'),
     path('post/<int:pk>/', post_detail, name='post_detail')
