7bba7f053520e9131d19962aa57fe82c09f36820
Kairat Bektov <64087609+internetwarrior@users.noreply.github.com>
2025-01-29 12:31:33 +0600

    Update 0.8.1

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
-  path('superman/', admin.site.urls),
+    path('superman/', admin.site.urls),
     path('', include('user.urls')),
     path('', include('post.urls')),
     path('api_v1/', include('api.urls')),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('privacy-policy/', TemplateView.as_view(template_name='privacy_policy.html'), name='privacy_policy'),
+    path('terms/', TemplateView.as_view(template_name='terms.html'), name='terms'),
+    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
